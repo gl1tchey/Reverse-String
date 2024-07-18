45b79a7dbb1328f4f50d3d91c0acda777baa668b
= <bontilaojiroh@gmail.com>
2024-07-19 07:13:42 +0800

    save

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class ReverseString
+{
+    public static void ReverseCharArray(char[] s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        char[] s1 = { 'h', 'e', 'l', 'l', 'o' };
+        ReverseCharArray(s1);
+    }
+}
